033b5e40c2861a8980be6af9dc13ea7fee3875ff
HalsSC <haruking.8686@gmail.com>
2020-03-29 01:19:41 +0900

    社会性フィルター

---
+++ README.md
@@ -0,0 +1,25 @@
+# social_filter
+社会性フィルターをCOTOHA APIを使って適用します
+
+## COTOHA_API.py
+COTOHA APIの操作ライブラリ
+例)以下のようにすることで固有表現の解析ができる
+~~~
+from COTOHA_API import CotohaApi
+
+DEVELOPER_API_BASE_URL = "https://api.ce-cotoha.com/api/dev/nlp/"
+ACCESS_TOKEN_PUBLISH_URL = "https://api.ce-cotoha.com/v1/oauth/accesstokens"
+CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXx"
+CLIENT_SECRET = "xxxxxxxxxxxxxxxxxx"
+
+cotoha_api = CotohaApi(CLIENT_ID,CLIENT_SECRET,DEVELOPER_API_BASE_URL,ACCESS_TOKEN_PUBLISH_URL)
+result = cotoha_api.ne(sentence)
+print(result['result'])
+~~~
+
+## social_filter.py
+社会性フィルターを適用するプログラム
+具体的には
+1. 固有表現を抽出し「その他」と判断されたもの以外すべて"にゃーん"に置きかえる
+2. 感情分析によりNegativeと判断した部分を"にゃーん"に置き換える
+3. 結果を出力
---
+++ social_filter.py
@@ -0,0 +1,39 @@
+# COTOHA APIの固有表現抽出APIを使用
+import os
+import urllib.request
+import json
+from COTOHA_API import CotohaApi
+
+DEVELOPER_API_BASE_URL = "https://api.ce-cotoha.com/api/dev/nlp/"
+ACCESS_TOKEN_PUBLISH_URL = "https://api.ce-cotoha.com/v1/oauth/accesstokens"
+CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXx"
+CLIENT_SECRET = "xxxxxxxxxxxxxxxxxx"
+
+meow = "にゃーん"
+
+if __name__ == '__main__':
+    # COTOHA APIインスタンス生成
+    cotoha_api = CotohaApi(CLIENT_ID, CLIENT_SECRET,DEVELOPER_API_BASE_URL, ACCESS_TOKEN_PUBLISH_URL)
+
+    # 解析対象文を入力
+    sentence = input("input>")
+    sen_old = sentence
+
+    # 固有表現解析API実行
+    result = cotoha_api.ne(sentence)
+
+    # 固有名詞の除去
+    for res in result['result']:
+        if res['class'] != "OTH":
+            sentence = sentence.replace(res['form'], meow)
+
+    # 感情分析API実行
+    result = cotoha_api.sentiment(sentence)
+
+    # マイナス(Negative)な表現の除去
+    for emo in result['result']['emotional_phrase']:
+        if emo['emotion'] in ["怒る", "悲しい", "不安", "恥ずかしい", "嫌", "N"]:
+            sentence = sentence.replace(emo['form'], meow)
+
+    # 整形して表示
+    print("\noutput>{}\n".format(sentence))
